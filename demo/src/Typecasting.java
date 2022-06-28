89cea0c35fbb5e8a177e48b8a6bc45cee126d0f3
lenovo <lenovo@DESKTOP-HJG1L1M>
2022-06-28 11:59:52 +0530

    Typecasting

---
+++ demo/src/Typecasting.java
@@ -0,0 +1,13 @@
+
+public class Typecasting {
+	public static void main(String[] args) {
+		int number = 10;
+		long l = (long)number;
+		double d2 = 1.234e2;
+		float point = (float)d2;
+		System.out.println(l);
+		System.out.println(point);
+		
+	}
+
+}
