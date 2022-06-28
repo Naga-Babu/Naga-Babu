7805ad769bea27153cb520f72a156326094da54e
lenovo <lenovo@DESKTOP-HJG1L1M>
2022-06-28 12:16:02 +0530

    Decisionmaking2

---
+++ demo/src/Decisionmaking2.java
@@ -0,0 +1,15 @@
+
+public class Decisionmaking2 {
+	public static void main(String[] args) {
+		int x = 8,y = 7;
+		if(x>=y)
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("false");
+		}
+	}
+
+}
