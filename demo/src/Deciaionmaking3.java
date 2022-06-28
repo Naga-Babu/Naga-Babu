be4944dc04927e8a818fcafe33d003addf717f3f
lenovo <lenovo@DESKTOP-HJG1L1M>
2022-06-28 13:02:21 +0530

    Decision making3

---
+++ demo/src/Deciaionmaking3.java
@@ -0,0 +1,15 @@
+
+public class Deciaionmaking3 {
+	public static void main(String[] args) {
+		int x = 10;int y = 11;
+		if(!(x<y)||(x==y))
+		{
+			System.out.println("Condition is true");
+		}
+		else
+		{
+			System.out.println("Condition is false");
+		}
+	}
+
+}
