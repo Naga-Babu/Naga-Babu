96be7f73473593a471a7e0909332c5bf11320123
lenovo <lenovo@DESKTOP-HJG1L1M>
2022-06-28 13:24:09 +0530

    Making decision with if else

---
+++ demo/src/Makingdecision.java
@@ -0,0 +1,30 @@
+
+public class Makingdecision {
+	public static void main(String[] args) {
+		int x = 5;
+		if (x==5)
+		{
+			if(x>=5)
+			{
+				System.out.println("x is greater than 5");
+			}
+			else
+			{
+				System.out.println("x is smaller then %");
+			}
+		}
+		if(x==5)
+		{
+			System.out.println("x equal to 5");
+		}
+		else if(x>5)
+		{
+			System.out.println("x is greater then to 5");
+		}
+		else
+		{
+			System.out.println("x is smaller than 5");
+		}
+	}
+
+}
