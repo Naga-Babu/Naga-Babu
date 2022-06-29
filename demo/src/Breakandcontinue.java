d52d20d3ebceab9595cf6c40c22552e0fa9ef14b
lenovo <lenovo@192.168.0.113>
2022-06-29 12:20:03 +0530

    Break and Continue

---
+++ demo/src/Breakandcontinue.java
@@ -0,0 +1,15 @@
+
+public class Breakandcontinue 
+{
+  public static void main(String[] args)
+  {
+	for(int i=1;i<=10;i++)
+	{
+		if(i==1)
+		{
+			continue;
+		}
+		System.out.println(i);
+	}
+}
+}
