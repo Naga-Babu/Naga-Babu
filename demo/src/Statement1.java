4b561f0d15a1d654d80b8aa34d37de375596ce95
lenovo <lenovo@192.168.0.113>
2022-06-29 10:53:14 +0530

    Switch case in java(char)

---
+++ demo/src/Statement1.java
@@ -0,0 +1,25 @@
+
+public class Statement1 {
+public static void main(String[] args) {
+	char x = 'F';
+	switch(x)
+	{
+	case'a':
+	case'A':
+		System.out.println("Value of x is a");
+		break;
+	case'b':
+	case'B':
+		System.out.println("Value of x is b");
+		break;
+	case'c':
+	case'C':
+		System.out.println("Value of x is c");
+		break;
+		default:
+		System.out.println("Value of x is a,b,c");
+		break;
+		
+	}
+}
+}
