ddb65876b14358437634eb32da7830be6dcf6c04
lenovo <lenovo@192.168.0.113>
2022-06-29 11:07:03 +0530

    Switch case in java(String)

---
+++ demo/src/Statement02.java
@@ -0,0 +1,20 @@
+
+public class Statement02 {
+public static void main(String[] args) {
+	String name = "ve";
+	switch(name.toLowerCase()) {
+	case "author":
+		System.out.println("Babu");
+		break;
+	case"svkp":
+		System.out.println("this is MCA Department");
+		break;
+	case"student":
+		System.out.println("Nagababu");
+		break;
+		default:
+			System.out.println("invalid entry");
+			break;
+	}
+}
+}
