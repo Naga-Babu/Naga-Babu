fa45350ab9d84f61ecb17d20ad6c4fda040cd9d8
Harika <Harika@192.168.0.107>
2022-07-23 10:51:50 +0530

    treeset

---
+++ demo/demo/src/Treeset.java
@@ -0,0 +1,23 @@
+import java.util.*;
+  
+
+class GFG {
+  
+
+    public static void main(String[] args)
+    {
+       
+        Set<String> tv1 = new TreeSet<>();
+  
+        
+        tv1.add("A");
+        tv1.add("B");
+        tv1.add("C");
+  
+
+        tv1.add("C");
+  
+      
+        System.out.println(tv1);
+    }
+}
